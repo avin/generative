ee43ecd37d4b615e59a1ea61fe1fea7a5bb855c4
Avin Grape <carcinogen75@gmail.com>
2020-02-10 02:01:17 +0300

    more stuff

---
+++ src/sketches/033_circle_behind_lines/shaders/frag.glsl
@@ -0,0 +1,141 @@
+#version 300 es
+#ifdef GL_ES
+    precision highp float;
+    precision highp int;
+    precision mediump sampler3D;
+#endif
+#define HW_PERFORMANCE 1
+uniform vec3      iResolution;
+uniform float     iTime;
+uniform float     iChannelTime[4];
+uniform vec4      iMouse;
+uniform vec4      iDate;
+uniform float     iSampleRate;
+uniform vec3      iChannelResolution[4];
+uniform int       iFrame;
+uniform float     iTimeDelta;
+uniform float     iFrameRate;
+struct Channel {
+    vec3  resolution;
+    float time;
+};
+uniform Channel iChannel[4];
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+void mainImage( out vec4 c, in vec2 f );
+
+// --------- START-SHADER-TOY-CODE-HERE ------------
+
+// Original one hosted on https://www.shadertoy.com/view/3ld3zN
+
+#define BLACK_COL vec3(16,22,26)/255.
+#define WHITE_COL vec3(235,241,245)/255.
+#define RED_COL vec3(245, 73, 139)/255.
+
+
+#define SF 1./min(iResolution.x,iResolution.y)
+
+#define MOD3 vec3(.1031,.11369,.13787)
+
+vec3 hash33(vec3 p3)
+{
+	p3 = fract(p3 * MOD3);
+    p3 += dot(p3, p3.yxz+19.19);
+    return -1.0 + 2.0 * fract(vec3((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y, (p3.y+p3.z)*p3.x));
+}
+
+float simplex_noise(vec3 p)
+{
+    const float K1 = 0.333333333;
+    const float K2 = 0.166666667;
+
+    vec3 i = floor(p + (p.x + p.y + p.z) * K1);
+    vec3 d0 = p - (i - (i.x + i.y + i.z) * K2);
+
+    vec3 e = step(vec3(0.0), d0 - d0.yzx);
+	vec3 i1 = e * (1.0 - e.zxy);
+	vec3 i2 = 1.0 - e.zxy * (1.0 - e);
+
+    vec3 d1 = d0 - (i1 - 1.0 * K2);
+    vec3 d2 = d0 - (i2 - 2.0 * K2);
+    vec3 d3 = d0 - (1.0 - 3.0 * K2);
+
+    vec4 h = max(0.6 - vec4(dot(d0, d0), dot(d1, d1), dot(d2, d2), dot(d3, d3)), 0.0);
+    vec4 n = h * h * h * h * vec4(dot(d0, hash33(i)), dot(d1, hash33(i + i1)), dot(d2, hash33(i + i2)), dot(d3, hash33(i + 1.0)));
+
+    return dot(vec4(31.316), n);
+}
+
+float tsin()
+{
+    float t = iTime * 1.25;
+	return sin(t + cos(t*2.))*.5 + .5;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 ouv = (fragCoord - iResolution.xy*.5)/iResolution.y;
+    vec2 q = fragCoord.xy / iResolution.xy;
+
+    float COUNT = floor(iResolution.y / 8.);
+    float wSize = 5.;
+
+    float sf = SF * COUNT * .75;
+
+    vec2 uv = ouv * vec2(1., COUNT);
+    vec2 gid = vec2(uv.x, floor(uv.y));
+    vec2 guv = vec2(uv.x, fract(uv.y))  - vec2(0., .5);
+
+    float g = 1.;
+    for(float i=-wSize; i<=wSize; i+=1.)
+    {
+        vec2 iuv = guv + vec2(0,i);
+        vec2 iid = gid - vec2(0,i);
+
+        vec2 nuv = iid / vec2(1., COUNT/5.);
+        vec2 uv = iuv + simplex_noise(vec3(nuv*COUNT/20., iTime*.5))*wSize/4. - vec2(0., wSize*.5);
+
+        float itsin = tsin()*.3;
+        float sss =
+            smoothstep(.1 + itsin,.2 + itsin, length(iid / vec2(1., COUNT)));
+        uv += vec2(0., sss )* wSize*1.25 ;
+
+        g *= smoothstep(abs(uv.y), .0 , sf*0.75);
+    }
+
+    float ctsin = tsin()*.3;
+    vec3 backCol = mix(BLACK_COL, RED_COL, smoothstep(.15+ctsin+SF, .15+ctsin, length(ouv)));
+
+    vec3 col = mix(WHITE_COL, backCol, g);
+
+
+
+    //-----------------------------------------------------
+    // postprocessing
+    //-----------------------------------------------------
+
+    // Border dark
+    col *= 0.2 + 0.8 * pow(32.0 * q.x * q.y * (1.0 - q.x) * (1.0 - q.y), 0.3);
+
+    // Fade in
+    col *= smoothstep(0.0, 1.0, iTime);
+
+    fragColor = vec4(col, 1.);
+}
+
+// --------- END-SHADER-TOY-CODE-HERE ------------
+
+out vec4 outColor;
+void main( void ) {
+    vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+    mainImage( color, gl_FragCoord.xy );
+    color.w = 1.0;
+    outColor = color;
+}
+
+// void main()
+// {
+//   mainImage(gl_FragColor, vUv * iResolution.xy);
+// }
