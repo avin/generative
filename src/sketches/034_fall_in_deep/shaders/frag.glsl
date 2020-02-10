cb0bda9c19ae47132a1f33e7643a52595d5da5b6
Avin Grape <carcinogen75@gmail.com>
2020-02-10 23:06:14 +0300

    more stuff

---
+++ src/sketches/034_fall_in_deep/shaders/frag.glsl
@@ -0,0 +1,66 @@
+#version 300 es
+#ifdef GL_ES
+    precision highp float;
+    precision highp int;
+    precision mediump sampler3D;
+#endif
+#define HW_PERFORMANCE 1
+uniform vec3      iResolution;
+uniform float     iTime;
+uniform float     iChannelTime[4];
+uniform vec4      iMouse;
+uniform vec4      iDate;
+uniform float     iSampleRate;
+uniform vec3      iChannelResolution[4];
+uniform int       iFrame;
+uniform float     iTimeDelta;
+uniform float     iFrameRate;
+struct Channel {
+    vec3  resolution;
+    float time;
+};
+uniform Channel iChannel[4];
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+void mainImage( out vec4 c, in vec2 f );
+
+// --------- START-SHADER-TOY-CODE-HERE ------------
+
+// Original one hosted on https://www.shadertoy.com/view/ttBSWc
+
+#pragma glslify: snoise3 = require(glsl-noise/simplex/3d)
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord - iResolution.xy*.5)/iResolution.y;
+
+    float sizeFactor = iResolution.y/100.;
+
+    float cc = 0.;
+    for(float i = 1.; i<1.1; i+=.01){
+        vec2 iuv = uv * i*1.1;
+        float s = snoise3(vec3(iuv*sizeFactor*i, iTime*.25 + i*5.));
+        s += iTime*.25 + i*100.;
+        cc += fract(floor(s*5.)/5.) * .1;
+    }
+
+    fragColor = vec4(fract(cc));
+}
+
+
+// --------- END-SHADER-TOY-CODE-HERE ------------
+
+out vec4 outColor;
+void main( void ) {
+    vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+    mainImage( color, gl_FragCoord.xy );
+    color.w = 1.0;
+    outColor = color;
+}
+
+// void main()
+// {
+//   mainImage(gl_FragColor, vUv * iResolution.xy);
+// }
