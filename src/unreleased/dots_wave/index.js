43954b6f6eae7de1af1131ecb1394f1b20c4fb24
Avin Grape <carcinogen75@gmail.com>
2020-03-01 16:13:15 +0300

    more stuff

---
+++ src/unreleased/dots_wave/index.js
@@ -0,0 +1,74 @@
+import fragmentShader from './shaders/frag.glsl';
+import vertexShader from './shaders/vert.glsl';
+
+const THREE = require('three');
+
+global.THREE = THREE;
+
+require('three/examples/js/controls/OrbitControls');
+
+const settings = {
+  animate: true,
+  context: 'webgl',
+};
+
+const sketch = ({ context, viewportWidth, viewportHeight }) => {
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas,
+  });
+  renderer.setClearColor('hsl(0, 0%, 98%)', 1);
+
+  const camera = new THREE.OrthographicCamera(-1, 1, -1, 1, 1, 1000);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  const particles = new THREE.PlaneGeometry(2.5, 2.5, 25, 25);
+  particles.translate(0, 0, -1);
+
+  const shaderPoint = THREE.ShaderLib.points;
+  const uniforms = {
+    ...shaderPoint.uniforms,
+    iTime: { value: 0 },
+    pSize: { value: 10 },
+    iResolution: { value: [viewportWidth, viewportHeight] },
+    // iResolution: THREE.Uniform(new THREE.Vector2([viewportWidth, viewportHeight])),
+  };
+
+  const pMaterial = new THREE.ShaderMaterial({
+    uniforms,
+    transparent: true,
+    depthWrite: false,
+
+    blending: THREE.NormalBlending,
+    fragmentShader,
+    vertexShader,
+  });
+
+  const particleSystem = new THREE.Points(particles, pMaterial);
+  particleSystem.sortParticles = true;
+
+  scene.add(particleSystem);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time, dimensions }) {
+      pMaterial.uniforms.iTime.value = time;
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      renderer.dispose();
+    },
+  };
+};
+
+export default { sketch, settings };
---
+++ src/unreleased/dots_wave/shaders/frag.glsl
@@ -0,0 +1,36 @@
+precision highp float;
+
+varying vec3 vpos;
+
+uniform vec2 iResolution;
+uniform float iTime;
+
+varying float fogDepth;
+varying float size;
+uniform float fogDensity;
+
+uniform mat3 uvTransform;
+
+#define PI 3.1415926
+#define TAU 6.2831852
+#define BLACK_COL vec3(24, 32, 38) / 255.
+
+#define rand1(p) fract(sin(p * 78.233) * 43758.5453)
+#define hue(h) clamp(abs(fract(h + vec4(3, 2, 1, 0) / 3.) * 6. - 3.) - 1., 0., 1.)
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = (uvTransform * vec3(gl_PointCoord.x, 1.0 - gl_PointCoord.y, 1)).xy - .5;
+
+    float l = length(uv);
+    float g = smoothstep(.5, .3, l);
+
+    fragColor = vec4(vec3(g * .1), g);
+}
+
+varying vec2 vUv;
+
+void main()
+{
+    mainImage(gl_FragColor, vUv * iResolution.xy);
+}
