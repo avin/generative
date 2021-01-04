8bc561173eb38847af03083540426a6461b66e7e
a.vinogradov <a.vinogradov@bpcbt.com>
2021-01-04 22:18:24 +0300

    fix mobile

--- src/sketches/053_linear_mushroom/shaders/tube/fragmentCustomDiffuse.glsl
+++ src/sketches/053_linear_mushroom/shaders/tube/fragmentCustomDiffuse.glsl
@@ -1,10 +1,11 @@
-float size = 110.;
+float size = 90.;
 
 vec2 fUV = vec2(vDiffuseUV.x, 0.);
 
 vec2 coord = fract(fUV * size);
 
-vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord) / 2.0;
+// vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord) / 2.0;
+vec2 grid = abs(fract(coord - .5) - .49999) / fwidth(coord) / 2.0;
 float line = min(grid.x, grid.y);
 
 line = smoothstep(.90, .10, line);
@@ -12,5 +13,3 @@ line = smoothstep(.90, .10, line);
 diffuseColor = vec3(1.);
 
 baseColor.rgb = mix(COL1, COL2, 1. - line);
-
-// diffuseColor = vDiffuseColor.rgb;
