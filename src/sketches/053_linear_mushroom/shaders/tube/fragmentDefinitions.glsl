60c39080d6e4a16a9a774f08204c05dbc944563b
a.vinogradov <a.vinogradov@bpcbt.com>
2021-01-04 11:04:17 +0300

    053_linear_mushroom

---
+++ src/sketches/053_linear_mushroom/shaders/tube/fragmentDefinitions.glsl
@@ -0,0 +1,7 @@
+#extension GL_OES_standard_derivatives : enable
+
+varying float vR;
+varying vec2 vUv;
+
+#define COL1 vec3(32, 43, 51) / 255.0
+#define COL2 vec3(235, 241, 245) / 255.0
---
+++ src/sketches/053_linear_mushroom/shaders/tube/vertexBeforePositionUpdated.glsl
@@ -0,0 +1,32 @@
+float t = 100. + iTime * 0.125;
+// float t = 10.;
+
+vUv = uv;
+vR = position.y;
+
+// float l = length(position.xz)*5.;
+
+
+
+//float pfX = sin(noise(vec3(vR*.025 + t, position.x*.05, 10.))) * 20.0;
+//float pfY = 0.;
+//float pfZ = sin(noise(vec3(vR*.025 + t-pfX*.1, position.z*.05+pfX*.1, 80.))) * 20.0;
+//
+//positionUpdated.x += pfX;
+//positionUpdated.y += pfY;
+//positionUpdated.z += pfZ;
+//
+//positionUpdated.xz *= (noise(vec3(vR*.05 + t, 0, 100.))*.5 + 1.0) * 1.0;
+
+float lf = (sin(positionUpdated.y)*.5+.5)*.5 + .5;
+positionUpdated.xz *= lf;
+
+float l = length(position.xy + t*10.);
+float nf1 = noise(vec3(vR*.5 + t, 0, 100.)) * 1.0;
+float yf = sin(l + lf*5.)*nf1;
+positionUpdated.y += yf;
+
+positionUpdated.x += sin(vR*0.1 + idx*PI)*8.;
+positionUpdated.z += cos(vR*0.1 + idx*PI)*8.;
+
+positionUpdated.y -= idx*3.0;
