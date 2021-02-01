07d102575bb149d8d455acd0cc908663f7b10e29
a.vinogradov <a.vinogradov@bpcbt.com>
2021-02-01 23:34:52 +0300

    071_elastic_cube

---
+++ src/sketches/071_elastic_cube/index.js
@@ -0,0 +1,129 @@
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { Scene } from '@babylonjs/core/scene';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { Quaternion, Vector3 } from '@babylonjs/core/Maths/math';
+import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
+import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera';
+import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
+import { DefaultRenderingPipeline } from '@babylonjs/core/PostProcesses/RenderPipeline';
+import { PBRCustomMaterial } from '@babylonjs/materials/custom/pbrCustomMaterial';
+import { VertexBuffer } from '@babylonjs/core/Meshes/buffer';
+import { VertexData } from '@babylonjs/core/Meshes/mesh.vertexData';
+
+const settings = {
+  animate: true,
+  context: 'webgl2',
+};
+
+const sketch = async ({ canvas, width, height }) => {
+  const engine = new Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+  });
+
+  //
+  // Main scene ===============================
+  //
+
+  const scene = new Scene(engine);
+  scene.clearColor = Color4.FromColor3(new Color3(25 / 255, 32 / 255, 38 / 255));
+
+  const cAlpha = Math.PI / 4;
+  const cBeta = Math.PI / 3;
+  const camera = new ArcRotateCamera('camera', cAlpha, cBeta, 3.0, new Vector3(0, 0, 0), scene);
+  camera.wheelPrecision = 50;
+  camera.minZ = 0.9;
+  camera.attachControl(canvas, true);
+
+  const baseLight = new HemisphericLight('hemiLight', new Vector3(-2, 2, 0), scene);
+  baseLight.diffuse = new Color3(1, 1, 1);
+  baseLight.specular = new Color3(0.0, 0.0, 0.0);
+
+  const mesh = MeshBuilder.CreateBox('box', { size: 1 }, scene);
+
+  mesh.increaseVertices(50);
+  mesh.markVerticesDataAsUpdatable(VertexBuffer.PositionKind, true);
+  mesh.markVerticesDataAsUpdatable(VertexBuffer.NormalKind, true);
+
+  const basePositions = new Float32Array(mesh.getVerticesData(VertexBuffer.PositionKind));
+
+  const updatePositions = (time, mesh, pl) => {
+    const positions = mesh.getVerticesData(VertexBuffer.PositionKind);
+
+    for (let i = 0; i < positions.length / 3; i += 1) {
+      const x = basePositions[i * 3 + 0];
+      const y = basePositions[i * 3 + 1];
+      const z = basePositions[i * 3 + 2];
+
+      const vec = new Vector3(x, y, z);
+
+      const t = time;
+
+      const pm = t | 0;
+
+      const dF = 0.2;
+
+      const pf = (t % 1) * (1 + dF);
+
+      if (pm % 2 !== 0) {
+        const rpf = Math.max(Math.min(pf + (z - 0.5) * dF, 1), 0);
+        const q = Quaternion.RotationAxis(new Vector3(0, 0, 1), (Math.cos(rpf * Math.PI) * Math.PI) / 2);
+        vec.rotateByQuaternionAroundPointToRef(q, new Vector3(0, 0, z), vec);
+      } else {
+        const rpf = Math.max(Math.min(pf + (y - 0.5) * dF, 1), 0);
+        const q = Quaternion.RotationAxis(new Vector3(0, 1, 0), (Math.cos(rpf * Math.PI) * Math.PI) / 2);
+        vec.rotateByQuaternionAroundPointToRef(q, new Vector3(0, y, 0), vec);
+      }
+
+      pl.chromaticAberration.aberrationAmount = Math.sin(((t / 2) % 1) * Math.PI * 2) * 80;
+
+      positions[i * 3 + 0] = vec.x;
+      positions[i * 3 + 1] = vec.y;
+      positions[i * 3 + 2] = vec.z;
+    }
+
+    mesh.updateVerticesData(VertexBuffer.PositionKind, positions);
+
+    const indices = mesh.getIndices();
+    const normals = [];
+    VertexData.ComputeNormals(positions, indices, normals);
+
+    mesh.updateVerticesData(VertexBuffer.NormalKind, normals);
+  };
+
+  const mat = new PBRCustomMaterial('plastic', scene);
+  // mat.wireframe = true;
+  mat.metallic = 0.0;
+  mat.roughness = 0.2;
+  mat.albedoColor = new Color3(1, 1, 1);
+  mesh.material = mat;
+
+  // -------------------
+
+  const defaultPipeline = new DefaultRenderingPipeline('default', true, scene, [camera]);
+  defaultPipeline.fxaaEnabled = true;
+  defaultPipeline.samples = 8;
+
+  defaultPipeline.imageProcessingEnabled = false;
+
+  defaultPipeline.chromaticAberrationEnabled = true;
+  defaultPipeline.chromaticAberration.aberrationAmount = 2.5;
+
+  // ------------------------------
+
+  return {
+    render({ time, width, height }) {
+      updatePositions(time * 0.5, mesh, defaultPipeline);
+
+      scene.render();
+    },
+    resize({ pixelRatio, width, height }) {
+      engine.resize();
+    },
+    unload() {
+      engine.dispose();
+    },
+  };
+};
+
+export default { sketch, settings };
