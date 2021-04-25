695febe104728496b1ab56638f02f482a2bf2f03
a.vinogradov <a.vinogradov@bpcbt.com>
2021-04-25 15:19:21 +0300

    commit

--- src/config.js
+++ src/config.js
@@ -81,6 +81,7 @@ const sketches = [
   '079_soft_touch',
   '080_black_sun',
   '081_dot_waves',
+  '082_knitting_pattern',
 ];
 /* END SKETCHES */
 
@@ -96,7 +97,6 @@ const expSketches = [
   'particles_tornado',
   'plane',
   'rotate',
-  'spiral',
   'three-exp',
   'torus',
   'yellow_snow',
---
+++ src/unreleased/076_trail_rotation2/shaders/tube/fragmentDefinitions.glsl
@@ -0,0 +1,10 @@
+varying vec3 vPos;
+
+vec3 hsv2rgb_smooth( in vec3 c )
+{
+    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
+
+	rgb = rgb*rgb*(3.0-2.0*rgb); // cubic smoothing
+
+	return c.z * mix( vec3(1.0), rgb, c.y);
+}
