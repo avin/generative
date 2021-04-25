40a48d5e140b6e6075de576a11a9c76c6e265ce7
a.vinogradov <a.vinogradov@bpcbt.com>
2021-04-25 15:18:04 +0300

    Revert "imp"
    This reverts commit 52cc62e0

--- src/sketches/082_knitting_pattern/shaders/mesh/fragmentCustomDiffuse.glsl
+++ src/sketches/082_knitting_pattern/shaders/mesh/fragmentCustomDiffuse.glsl
@@ -1,6 +1,6 @@
-vec2 uv = vDiffuseUV + uvOffset;
+vec2 uv = vDiffuseUV+uvOffset;
 float cDot = dot(normalW, normalize(camPos));
-float bz = smoothstep(1.0, .0, fract(uv.y * 2.));
+float bz = smoothstep(1.0, .0, fract(uv.y*2.));
 
-baseColor.rgb = hsv2rgb_smooth(vec3(uv.y + bz + iFrame * .001, .75, 1.));
-baseColor *= smoothstep(1.5, .0, cDot * 1.1);
+baseColor.rgb = hsv2rgb_smooth(vec3(uv.y + bz + iFrame*.001, .75, 1.));
+baseColor *= smoothstep(1.5, .0, cDot*1.1);
