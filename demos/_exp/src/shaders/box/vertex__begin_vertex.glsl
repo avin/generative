b961a74eec25a179c26bca13a318424b6e5d5b1d
a.vinogradov <a.vinogradov@bpcbt.com>
2022-01-12 15:29:43 +0300

    upd

--- demos/_exp/src/shaders/box/vertex__begin_vertex.glsl
+++ demos/_exp/src/shaders/box/vertex__begin_vertex.glsl
@@ -5,16 +5,17 @@ vec3 spherePos = boxPosToSpherePos(pos, radius);
 // float t = sin(iTime) * .5 + .5;
 float t = 1.;
 
+float fY = rand(vec2(offset.x*.0033, offset.z*.00175))*.5 - .25;
+float fS = rand(vec2(offset.x*.0561, offset.z*.0217)) * PI2 + iTime;
 
-
-float moveTime = iTime*1.;
+float moveTime = sin(fS + iTime)*.5 + .5 + iTime;
 float moveY = fract(moveTime)*2.;
 
 t = clamp((offset.y + moveY) / 7.5, 0., 1.);
 // t = 0.;
 vec3 transformed = mix(pos, spherePos, t);
 
-vec3 move = vec3(0., moveY, 0.);
+vec3 move = vec3(0., moveY + fY, 0.);
 
 transformed += offset + move;
 
